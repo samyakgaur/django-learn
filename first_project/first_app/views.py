ca7943cd104088794ff49ee7644b8350234904f0
Samyak Gaur <samyakgaur@yahoo.com>
2019-06-11 00:25:50 +0530

    made template folder to learn about django template - samyak gaur

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -4,5 +4,11 @@ from django.http import HttpResponse
 # This will display the stuff written here when this is called from the project/url 
 
 #creating a function 
+# def index(request):
+#     return HttpResponse('Hello World')
+
+#using render function now to learn templates 
+
 def index(request):
-    return HttpResponse('Hello World')
+    my_dict={'insert_me':"Hello this is from views.py"}
+    return render(request,'first_app/index.html',context=my_dict)
--- first_project/first_project/settings.py
+++ first_project/first_project/settings.py
@@ -14,7 +14,12 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_DIR = os.path.join(BASE_DIR,"templates")
 
+
+#------->this is using python to make our project platform independent and 
+#       this prints path regardless of the machine we are working on 
+#       print(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -56,7 +61,7 @@ ROOT_URLCONF = 'first_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
