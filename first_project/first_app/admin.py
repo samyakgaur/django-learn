6c3ac770eaf2bfb7d45272f7754cbb35e9096a6e
Samyak Gaur <samyakgaur@yahoo.com>
2019-06-12 20:59:53 +0530

    registered admin function for models - samyak gaur

--- first_project/first_app/admin.py
+++ first_project/first_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+#importing the models for the admin file to load 
+from first_app.models import Topic , AccessRecord , Webpage
 
 # Register your models here.
+
+admin.site.register(Topic)
+admin.site.register(AccessRecord)
+admin.site.register(Webpage)
